22dc5a24e95d3dcd439ab723b48bb13413e3ea64
Kostadin Markov <kostadin.markov@rewe-group.com>
2025-11-12 17:58:48 +0200

    todo list

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,53 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {MatList, MatListItem} from '@angular/material/list';
+import {IItem} from './types/item.type';
+import {FormsModule} from '@angular/forms';
+import {MatFormField, MatInput} from '@angular/material/input';
+import {MatButton} from '@angular/material/button';
+import {MatCheckbox} from '@angular/material/checkbox';
+import {NgStyle} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule, MatInput, MatFormField, MatButton, MatCheckbox, MatList, MatListItem, NgStyle],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('todo-list');
+  items: IItem[] = [];
+  value: string = '';
+  containItem: boolean = false;
+
+  addItem() {
+    this.items.push({name: this.value, selected: false, hidden: false});
+    this.value = '';
+    for (let i = this.items.length - 1; i >= 0; i--) {
+      this.items[i].hidden = false;
+    }
+  }
+
+  removeItems() {
+    for (let i = this.items.length - 1; i >= 0; i--) {
+      if (this.items[i].selected) {
+        this.items.splice(i, 1);
+      }
+    }
+  }
+
+  filterItems() {
+    if (this.value == '') return;
+    this.containItem = false;
+    for (let i = this.items.length - 1; i >= 0; i--) {
+      if (this.items[i].name == this.value) {
+        this.items[i].hidden = false;
+        this.containItem = true;
+      } else {
+        this.items[i].hidden = true;
+      }
+    }
+  }
+
+  updateItem(checked: boolean, item: IItem) {
+    item.selected = checked;
+  }
 }
---
+++ src/app/types/item.type.ts
@@ -0,0 +1,5 @@
+export interface IItem {
+  name: string,
+  hidden: boolean,
+  selected: boolean
+}
