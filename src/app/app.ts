1d6f1e3da5284b69782ca07a05fc635814c3f19d
Kostadin Markov <kostadin.markov@rewe-group.com>
2025-11-14 16:28:17 +0200

    disable delete button if nothing is selected

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,35 +5,37 @@ import {FormsModule} from '@angular/forms';
 import {MatFormField, MatInput} from '@angular/material/input';
 import {MatButton} from '@angular/material/button';
 import {MatCheckbox} from '@angular/material/checkbox';
-import {NgStyle} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [FormsModule, MatInput, MatFormField, MatButton, MatCheckbox, MatList, MatListItem, NgStyle],
+  imports: [FormsModule, MatInput, MatFormField, MatButton, MatCheckbox, MatList, MatListItem],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   items: IItem[] = [];
+  selectedItems: IItem[] = [];
   value: string = '';
   enableAdd: boolean = false;
 
   addItem() {
     this.items.push({name: this.value, selected: false, hidden: false});
-    this.showAllItems();
+    this.resetSearch();
   }
 
   removeItems() {
-    for (let i = this.items.length - 1; i >= 0; i--) {
-      if (this.items[i].selected) {
-        this.items.splice(i, 1);
-      }
+    for (let i = this.selectedItems.length - 1; i >= 0; i--) {
+      this.items.splice(this.items.indexOf(this.selectedItems[i]), 1);
+      this.selectedItems.splice(i, 1);
     }
-    this.showAllItems();
+    this.resetSearch();
   }
 
   filterItems() {
-    if (this.value == '') return;
+    if (this.value == '') {
+      this.resetSearch();
+      return;
+    }
     this.enableAdd = false;
     for (let i = this.items.length - 1; i >= 0; i--) {
       if (this.items[i].name == this.value) {
@@ -45,12 +47,19 @@ export class App {
     }
   }
 
-  updateItem(checked: boolean, item: IItem) {
-    item.selected = checked;
+  selectItem(checked: boolean, item: IItem) {
+    item.selected = checked
+    if (checked) {
+      this.selectedItems.push(item);
+    } else {
+      this.selectedItems.splice(this.selectedItems.indexOf(item), 1);
+    }
   }
 
-  showAllItems() {
-    this.value = '';
+  resetSearch() {
+    if (this.value != '') {
+      this.value = '';
+    }
     for (let i = this.items.length - 1; i >= 0; i--) {
       this.items[i].hidden = false;
     }
